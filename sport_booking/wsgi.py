2f04ec52a7710b416a9458554a1a498a668892e6
jonayoung <young.jon@icloud.com>
2021-01-09 16:45:27 -0500

    changed directory name

--- sport_booking/wsgi.py
+++ sport_booking/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sport_booking project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sport_booking.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
